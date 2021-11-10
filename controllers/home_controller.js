b73735e0cba00cd8a46b206fea235a958322f31f
Jaskirat Singh Saggu <saggu.jaskiratsingh@gmail.com>
2021-11-10 22:16:48 +0530

    displayed comments with each post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -12,8 +12,15 @@ module.exports.home=function(req,res){
     // });
 
     //populate the user of each post
-    Post.find({}).populate('user').exec(function(err,posts){
-
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec(function(err,posts){
         if(err){console.log("error in showing posts "); return;}
         return res.render('home',{
             title:"home" ,
