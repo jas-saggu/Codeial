b51bee96ad624c04017dfeef4a56caf83aca4439
Jaskirat Singh Saggu <saggu.jaskiratsingh@gmail.com>
2021-11-11 15:26:06 +0530

    add option to delete post and all comments associated to that post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post');
+const Comment=require('../models/comment')
 
 module.exports.create=function(req,res){
     Post.create({
@@ -10,3 +11,20 @@ module.exports.create=function(req,res){
         return res.redirect('back');
     });
 }
+
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        // post.user is the id of the user who created that post(string)
+        // req.user.id has the id of user who is trying to delete
+        // .id & id is different as id returns a string
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id},function(err){
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    });
+};
--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,6 @@ const postController=require('../controllers/posts_controller');
 
 // user can only make a post if user is signed in
 router.post('/create',passport.checkAuthentication,postController.create);
+router.get('/destroy/:id',passport.checkAuthentication,postController.destroy);
 
 module.exports=router;
