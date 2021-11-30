d1d0cffbb77bd2271f4674577261a9203c29117c
Jaskirat Singh Saggu <saggu.jaskiratsingh@gmail.com>
2021-11-30 15:49:58 +0530

    set up jwt authentication and authorisation

--- controllers/api/v1/post.api.js
+++ controllers/api/v1/post.api.js
@@ -23,12 +23,18 @@ module.exports.destroy=async function(req,res){
 
     try{
        let post=await Post.findById(req.params.id);
+       if(post.user== req.user.id){
             post.remove();
             await Comment.deleteMany({post: req.params.id})
 
             return res.json(200,{
                 message:'post and assoicated comments deleted'
             })
+       }else{
+           return res.json(401,{
+                message:'you cannot delete this post'
+           });
+       }
 
     }catch(err){
         console.log('')
