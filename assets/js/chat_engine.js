6cfef08e6d622964da60299737a7877a1894e230
Jaskirat Singh Saggu <saggu.jaskiratsingh@gmail.com>
2021-12-07 19:28:03 +0530

    install web socket and set up chat engine

---
+++ assets/js/chat_engine.js
@@ -0,0 +1,75 @@
+// communicating from client side/ user 
+class ChatEngine{
+    constructor(chatBoxId,userEmail){
+        this.chatBox=$(`#${chatBoxId}`);
+        this.userEmail=userEmail;
+
+        //fires io.connection in chat_sockets(sends connection request)
+        this.socket=io('http://localhost:5000', { transports: ['websocket', 'polling', 'flashsocket'] });
+        if(this.userEmail){
+            this.connectionHandler();
+        }
+    }
+    //runs when io.connection is executed
+    connectionHandler(){
+
+        let self=this;
+
+
+        this.socket.on('connect',function(){
+            console.log('connection established using socket');
+
+            self.socket.emit('join_room',{
+                user_email:self.userEmail,
+                chatroom:'PersonalRoom'
+            })
+
+            self.socket.on('user_joined',function(data){
+                console.log('a user joined ',data);
+            })
+
+        });
+
+        $('#send-message').click(function(){
+            let msg=$('#chat-message-input').val();
+            if(msg!=''){
+                self.socket.emit('send_message',{
+                    message:msg,
+                    user_email:self.userEmail,
+                    chatroom:'PersonalRoom'
+                });
+            }
+        });
+        
+        self.socket.on('receive_message',function(data){
+            console.log('message received ',data.message);
+
+            let newMessage=$('<li>');// create a li
+            
+            // identify what class it will have depending on who wrote the message
+            let messageType='other-message';
+
+            // if its a self message i.e i only wrote it
+            // My message coming back to me is an indication that everyone else has recieved the message
+            if(data.user_email==self.userEmail){
+                messageType='self-message';
+            }
+
+            // add a span
+            newMessage.append($('<span>',{
+                'html':data.message
+            }));
+            // add user_email to make it easy to identify who wrote the message
+            newMessage.append($('<sub>',{
+                'html':data.user_email
+            }));
+
+            // add the class 
+            newMessage.addClass(messageType);
+
+            // finally append it to message list
+            $('#chat-messages-list').append(newMessage);
+        })
+    }
+
+}
---
+++ config/chat_sockets.js
@@ -0,0 +1,34 @@
+// observer/ server who will recieve the incoming connections from all the users(listners/subscribers)
+
+//.on -> detects event sent by client
+module.exports.chatSockets =function(socketServer){
+    let io = require("socket.io")(socketServer, {
+        cors: {
+          origin: "http://localhost:8080",
+          methods: ["GET", "POST"]
+        }
+      });
+
+    io.sockets.on('connection',function(socket){
+        console.log('new connection received',socket.id);
+
+        socket.on('disconnect',function(){
+            console.log('socket disconnected ');
+        })
+
+        socket.on('join_room',function(data){
+            console.log('joining req rec : ',data);
+            // data.chatroom is the name of chatroom( if it exist it joins the user, if it doesnt exist it creates)
+            socket.join(data.chatroom);
+
+            // sends a notification to all the other users in the room that a new user has joined the chatroom
+            io.in(data.chatroom).emit('user_joined',data);
+
+        });
+
+        socket.on('send_message',function(data){
+            io.in(data.chatroom).emit('receive_message',data);
+        })
+    });
+    
+};
