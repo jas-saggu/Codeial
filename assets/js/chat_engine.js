7648e94a0bd29ad35a72598356e086b0e27c83d5
Jaskirat Singh Saggu <saggu.jaskiratsingh@gmail.com>
2021-12-15 15:46:38 +0530

    update

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -5,8 +5,7 @@ class ChatEngine{
         this.userEmail=userEmail;
 
         //fires io.connection in chat_sockets(sends connection request)
-        // this.socket=io('http://54.88.21.79:5000/rooms', { transports: ['websocket'] });
-        this.socket = io.connect('http://54.88.21.79:5000');
+        this.socket=io('http://54.88.21.79:5000/rooms', { transports: ['websocket'] });
         if(this.userEmail){
             this.connectionHandler();
         }
